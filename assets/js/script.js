a372412846219d5ad18559574381cf43b69d3112
Audry Ford <audryford792@gmail.com>
2021-07-13 12:33:07 -0500

    add content to html and outline js

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Click the start button
+
+
+
+// The landing page should hide
+
+
+
+// First question will appear
+// Timer will display and start counting down from 60 seconds (function for timer?)
+
+
+
+
+// User chooses their answer
+    // If the answer is correct, then display 'correct' and show next question
+
+
+
+    // If the answer is incorrect, then display 'incorrect', deduct 10 seconds from timer, redisplay time, and show next question
+
+
+
+// After all questions are answered, user will enter their initials
+
+
+
+// Once they enter thier initials, they will be taken to the highscores page
+
+
+
