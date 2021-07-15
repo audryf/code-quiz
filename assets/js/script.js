f342411577ae8caf9ae53f852da95bfbdad9f048
Audry Ford <audryford792@gmail.com>
2021-07-15 11:37:41 -0500

    show timer and display questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,3 @@
-var currentQuestion = 0
-var timeLeft = 60
-
-
-var displayQuestions = function () {
-    var questionsEl = document.getElementById("questions");
-    var choicesEl = document.getElementsByClassName("choices");
-
-    questionsEl.textContent = quizQuestions[currentQuestion].question;
-
-    for (var i = 0; i < choicesEl.length; i++) {
-        choicesEl[i].innerHTML = quizQuestions[currentQuestion].choices[i];
-        choicesEl[i].addEventListener("click", function(event) {
-            var clicked = event.target.innerHTML;
-
-            if (clicked === quizQuestions[currentQuestion].answer) {
-                alert("Correct");
-                currentQuestion++;
-                
-                displayQuestions();
-            } else {
-                alert("Incorrect!!!!!!");
-                currentQuestion++;
-                displayQuestions();
-            }
-        });
-    };
-
-    
-};
-
-
-// Start quiz function
-var startQuiz = function () {
-    // The landing page should hide
-    var hiddenEl = document.getElementById("to-hide");
-    hiddenEl.classList.add("hide");
-
-    var show = document.getElementById("question-container");
-    show.classList.toggle("hide");
-
-    displayQuestions();
-
-    var timer = setInterval(function(){ 
-        timeLeft--;
-        // console.log(timeLeft);
-        if (timeLeft === 0) {
-            clearInterval(timer);
-            show.classList.toggle("hide");
-        } 
-    }, 1000);
-};
-
-
-// Click the start button
-var startButton = document.getElementById("startButton");
-startButton.addEventListener("click", startQuiz);
-
-
-// Timer will display and start counting down from 60 seconds (function for timer?)
-
-
-
-
-// User chooses their answer
-// If the answer is correct, then display 'correct' and show next question
-
-
-
-// If the answer is incorrect, then display 'incorrect', deduct 10 seconds from timer, redisplay time, and show next question
-
-
-
-// After all questions are answered, user will enter their initials
-
-
-
-// Once they enter thier initials, they will be taken to the highscores page
-
-
-
-
-
-// Questions array
 var quizQuestions = [
     // [0]
     {
@@ -102,3 +18,62 @@ var quizQuestions = [
         answer: "The code between curly braces"
     }
 ]
+
+var timeLeft = 61
+var displayTime = document.createElement("div");
+var show = document.getElementById("question-container");
+
+
+var displayQuestions = function (currentQuestion) {
+    var questionsEl = document.getElementById("questions");
+    var choicesEl = document.getElementsByClassName("choices");
+    console.log(currentQuestion);
+    
+    questionsEl.textContent = quizQuestions[currentQuestion].question;
+    for (var i = 0; i < choicesEl.length; i++) {
+        choicesEl[i].innerHTML = quizQuestions[currentQuestion].choices[i];
+        
+        choicesEl[i].addEventListener("click", function(event) {
+            // User chooses their answer
+            var clicked = event.target.innerHTML;
+            // If the answer is correct, then display 'correct' and show next question  
+            if (clicked != quizQuestions[currentQuestion].answer && currentQuestion < (quizQuestions.length -1)) {
+                console.log("wrong");
+                timeLeft = timeLeft - 10;
+                currentQuestion++;
+                displayQuestions(currentQuestion);
+            } 
+            
+        });
+    };
+};
+
+// Start quiz function
+var startQuiz = function () {
+    // The landing page should hide
+    var hiddenEl = document.getElementById("to-hide");
+    hiddenEl.classList.add("hide");
+    var currentQuestion = 0
+    
+    show.classList.toggle("hide");
+    displayQuestions(currentQuestion);
+ 
+};
+
+ // Timer will display and start counting down from 60 seconds
+ var timer = setInterval(function(){ 
+    timeLeft--;
+    displayTime.textContent = "Time Left: " + timeLeft;
+    show.appendChild(displayTime);
+    
+    // console.log(timeLeft);
+    if (timeLeft === 0) {
+        clearInterval(timer);
+        // show.classList.toggle("hide");
+    } 
+}, 1000);
+
+// Click the start button
+var startButton = document.getElementById("startButton");
+startButton.addEventListener("click", startQuiz);
+
